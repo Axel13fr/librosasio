bcd36aa492fa2695e1b38b401aec5c4d7e47b1bc
Robin Vanhove <rov@robopec.com>
2020-11-24 11:03:39 +0100

    fix typo

--- examples/timers/main.cpp
+++ examples/timers/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
 #if BOOST_VERSION <= 106501
   boost::asio::io_service io_context;
 #else
-  boost::asio::io_context m_io_context;
+  boost::asio::io_context io_context;
 #endif
 
   // Static Function Call doing the magic to unify event loops
