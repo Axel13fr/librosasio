de133213ee6a3e59c888d80f994e61a6903daa89
Robin Vanhove <rov@robopec.com>
2020-11-24 11:25:02 +0100

    in example : remove drift from boost timer

--- examples/timers/node.cpp
+++ examples/timers/node.cpp
@@ -9,8 +9,9 @@ TestNode::TestNode(boost::asio::io_service& io_context, ros::NodeHandle &_nh):
     m_boost_timer(io_context)
 {
     m_start_stamp = std::chrono::steady_clock::now();
+    m_expected_timer_stamp = m_start_stamp + 100ms;
 
-    m_boost_timer.expires_from_now(100ms);
+    m_boost_timer.expires_at(m_expected_timer_stamp);
     m_boost_timer.async_wait([this](const boost::system::error_code& error){onAsioTimer(error);});
 
     m_periodic_timer = _nh.createTimer(ros::Duration(0.1),
@@ -27,12 +28,17 @@ void TestNode::onAsioTimer(const boost::system::error_code& error)
 {
     if(!error)
     {
-        m_boost_timer.expires_from_now(100ms);
-        
         auto now = std::chrono::steady_clock::now();
         auto usStamp = std::chrono::duration_cast<std::chrono::microseconds>(now - m_start_stamp).count();
         std::cout << "@ " << usStamp/1000. << "\tms : boost deadline timer CB from thread :\t" << std::this_thread::get_id() << "\n";
         
+        m_expected_timer_stamp += 100ms;
+        if(std::chrono::steady_clock::now() > m_expected_timer_stamp)
+        {
+            // in case processing took a very long time
+            m_expected_timer_stamp = now + 100ms;
+        }
+        m_boost_timer.expires_at(m_expected_timer_stamp);
         m_boost_timer.async_wait([this](const boost::system::error_code& error){onAsioTimer(error);});
     }
     else
--- examples/timers/node.h
+++ examples/timers/node.h
@@ -17,4 +17,5 @@ private:
     boost::asio::steady_timer m_boost_timer;
     ros::Timer m_periodic_timer;
     std::chrono::steady_clock::time_point m_start_stamp;
+    std::chrono::steady_clock::time_point m_expected_timer_stamp;
 };
